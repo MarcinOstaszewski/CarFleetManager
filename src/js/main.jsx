ac3b23176b2b9f844db4ac17797578edfe56320f
MarcinOstaszewski <marcinostaszewski@gmail.com>
2018-02-08 19:45:14 +0100

    update main.jsx

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Routing} from './components/routing.jsx';
 
 require('../sass/main.scss');
 
@@ -11,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
         render() {
             return (
                 <div>
-                    <h1>Hello!</h1>
+                    <Routing/>
                 </div>
             )
         }
